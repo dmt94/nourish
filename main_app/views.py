0d660ffcb829524f17693263f34b4366b60913d2
GiselleMR <giselle.rubin1@gmail.com>
2023-03-21 09:47:04 -0400

    add html and refactor lists

--- main_app/views.py
+++ main_app/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Restaurant, Review
-import googlemaps
+from googlemaps import Client as googlemaps
 
 # Create your views here.
 def home(request):
