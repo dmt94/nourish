83c828da633b06790ee947b38348a6f8469412aa
sumurillo <sumire.ohana@gmail.com>
2023-03-21 09:18:55 -0500

    conflict resolve

--- main_app/views.py
+++ main_app/views.py
@@ -9,7 +9,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Restaurant, Review
-import googlemaps
 
 # Create your views here.
 def home(request):
