0dfb37c81127ce67dd695bd70b4284e1cbf76a92
adalawson96 <adalawson96@gmail.com>
2023-03-21 14:48:04 +0100

    add google view import

--- main_app/views.py
+++ main_app/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Restaurant, Review
-import googlemaps
+from googlemaps import Client as cl
 
 # Create your views here.
 def home(request):
