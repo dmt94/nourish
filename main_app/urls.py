85ae2c6e12f8580b61d5d119874e874344ec7236
sumurillo <sumire.ohana@gmail.com>
2023-03-21 10:06:40 -0500

    created favorites page, nav, path

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
   path('accounts/signup', views.signup, name='signup'),
   path('restaurants/', views.RestaurantList.as_view(), name='index'),
   path('restaurants/create/', views.RestaurantCreate.as_view(), name='restaurant_create'),
-  # path('favorites/', views.restaurants_index, name='index'),
+  path('favorites/', views.Favorites.as_view(), name='favorites'),
   path('restaurants/<int:restaurant_id>/', views.detailsview, name='detail'),
 ]
