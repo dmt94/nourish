0fa4f92f9026da48dd7a115e6830e62f29165330
Daevah Taylaran <daevtayl@gmail.com>
2023-03-23 12:13:34 -0700

    Config deployment

--- nourish/settings.py
+++ nourish/settings.py
@@ -132,3 +132,9 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Other settings above
+
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals(), databases=False)
